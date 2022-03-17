96c44eb043a9c8bb76f33ca516199c457d085856
Goel <Akshay.Goel@cbre.com>
2022-03-17 22:40:50 +0530

    added new logic and event from parent to child and viceversa

---
+++ src/components/TwootItem.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="twoot-item" @click="favTwitte(twoot.id)"> <!--to pass the data from child to parent -->
+    <div class="user-profile__twoot">
+      <div class="twoot-item__user">
+        @{{ username }}
+      </div>
+      <div class="twoot-item__content">
+        {{ twoot.content }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TwootItem",
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+    twoot: {
+      type: Object,
+      required: true
+    }
+  },
+  methods:{
+    favTwitte(id){
+      this.$emit('favourite', id) //to pass the data back to parent favourite becomes a tag
+    }
+  }
+};
+</script>
+
+<style>
+.twoot-item {
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3E8;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
+  }
+
+  .twoot-item:hover {
+    transform: scale(1.1, 1.1);
+  }
+
+  .twoot-item__user {
+    font-weight: bold;
+  }
+
+</style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,47 +1,67 @@
 <template>
-  <div class="user-profile">
-   <div class="user-profile__user-panel">
-    <h1 class="user-profile__username"> @{{user.name}} </h1>
-    <div class="user-profile__follower-count">
-    <strong>followers : </strong> {{follower}}
-    <br>
-    <!-- <button v-on:click="followUser">Follow</button> -->
-    <button @click="followUser">Follow</button>
-    <button @click="clearAll">ClearAll</button>
+<div class="user-profile">
+    <div class="user-profile__user-panel">
+        <h1 class="user-profile__username"> @_{{user.name}} </h1>
+        <div v-if="user.isAdmin" class="user-profile__admin-badge">
+            Admin
+        </div>
+        <div v-else class="user-profile__admin-badge">
+            Non Admin
+        </div>
+        <div class="user-profile__follower-count">
+            <strong>followers : </strong> {{follower}}
+            <!-- <br>
+          <button v-on:click="followUser">Follow</button> 
+            <button @click="followUser">Follow</button>
+            <button @click="clearAll">ClearAll</button> -->
+        </div>
     </div>
-  </div>
-
-  </div>
+    <div class="user-profile__twitte-wrapper">
+    <TwootItem v-for="twitte in user.twittes" :key="twitte.id" :username="user.name" :twoot="twitte" @favourite="toggleFavourite"/>
+    </div>
+</div>
 </template>
 
 <script>
-
-export default{
-    name : 'UserProfile',
+import TwootItem from './TwootItem.vue'
+export default {
+    name: 'UserProfile',
+    components: {
+        TwootItem
+    },
     data() {
         return {
             follower: 0,
             user: {
-                name: 'AKG',
+                name: 'a_k_goel',
                 firstName: 'Akshay',
                 lastName: 'Goel',
                 email: 'akgoeltech@gmail.com',
-                isAdmin: true
+                isAdmin: true,
+                twittes: [{
+                        id: 1,
+                        content: "twitter is amazing!!! "
+                    },
+                    {
+                        id: 2,
+                        content: "One more twitte : please suscribe "
+                    }
+                ]
             }
         }
     },
-    watch:{
-  //used when you want to trigger something if someting happens
-  //i.e : when you have new follwer you want to send notification 
-   //we can also watch comuted property
-   follower(newCount, oldCount){
-     if(oldCount < newCount){
-       console.log(`${this.fullName} gained the follower`)
-     }else{
-       console.log(`${this.fullName} lost the follower`)
-     }
+    watch: {
+        //used when you want to trigger something if someting happens
+        //i.e : when you have new follwer you want to send notification 
+        //we can also watch comuted property
+        follower(newCount, oldCount) {
+            if (oldCount < newCount) {
+                console.log(`${this.fullName} gained the follower`)
+            } else {
+                console.log(`${this.fullName} lost the follower`)
+            }
 
-   }
+        }
 
     },
     computed: {
@@ -50,40 +70,52 @@ export default{
         }
     },
     methods: {
-        followUser(){
-          this.follower++;
+        followUser() {
+            this.follower++;
         },
-        clearAll(){
-          this.follower = 0
+        clearAll() {
+            this.follower = 0
+        },
+        toggleFavourite(id){
+            console.log(`id is retruned backed to parent from child  #${id}`)
         }
     },
-    mounted(){
-      this.followUser()
+    mounted() {
+        this.followUser()
     }
 }
 </script>
 
 <style>
-
 .user-profile {
-display: grid;
-grid-template-columns: 1fr 3fr;
-width: 100;
-padding: 50px 5%;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100;
+    padding: 50px 5%;
 }
 
 .user-profile__user-panel {
 
-display: flex;
-flex-direction: column;
-margin-right: 50px;
-padding: 20px;
-background-color: white;
-border-radius: 5px;
-border: 1px solid #DFE3E8
-}
-h1{
-margin: 0;
+    display: flex;
+    flex-direction: column;
+    margin-right: auto;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 2px solid #94bdee;
+    justify-content: center
 }
 
+h1 {
+    margin: 0;
+}
+
+.user-profile__admin-badge {
+    background: purple;
+    color: white;
+    margin-right: auto;
+    border-radius: 5px;
+    font-weight: bold;
+
+}
 </style>
